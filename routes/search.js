d930d39a7093ddc531c08895996e70a412e1918c
Ivan Thai <ithai@box.com>
2022-06-28 16:52:10 -0700

    return results correctly

--- routes/search.js
+++ routes/search.js
@@ -12,7 +12,9 @@ const API_HANDLERS = {
 const MAX_RESULTS = 25;
 
 const getResponse = (search, appNames) => {
-	return Promise.all(Object.keys(API_HANDLERS).filter(key => appNames.includes(key)).map(name => API_HANDLERS[name]()));
+	return Promise.all(Object.keys(API_HANDLERS).filter(key => appNames.includes(key)).map(name => API_HANDLERS[name]().then(results => {
+		return {name, results};
+	})));
 }
 
 /* GET search results */
