a8aa667af74e057735d69f1e55c989ee61bc5ad0
Yixuan Qian <qianyixuaneric@gmail.com>
2022-06-28 15:21:49 -0700

    stack overflow POC

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = express();
 server.use(cors());
-
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
     server.use(express.static('frontend/build'));
@@ -16,6 +22,30 @@ if (process.env.NODE_ENV === 'production') {
         res.json('hello');
     });
 }
+
+server.post('/api/search/stack-overflow', (req, res) => {
+    const searchText = req.body.searchText;
+    console.log(searchText);
+    const team = 'stackoverflow.com/c/enterprise-at-box';
+    const key = process.env.STACK_OVERFLOW_KEY;
+    const accessToken = process.env.STACK_OVERFLOW_ACCESS_TOKEN;
+    const url = `https://api.stackexchange.com/2.3/search/excerpts?order=desc&sort=activity&title=${searchText}&site=stackoverflow&key=${key}&team=${team}`;
+    const config = {
+        headers: {
+            'X-API-Access-Token': accessToken
+        }
+    };
+    axios.get(url, config).then((response) => {
+        const data = response.data;
+        data.items.forEach(item => {
+            if (item.item_type === 'question') {
+                const qid = item.question_id;
+                item.link = `https://stackoverflow.com/c/enterprise-at-box/questions/${qid}`;
+            }
+        });
+        res.json(data);
+    });
+});
 
 const port = process.env.PORT || 5000;
 
